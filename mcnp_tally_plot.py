bc2ba0d0f5e0b844a94e6f044c369416931f2f1f
dusankr <kral.dusan0@gmail.com>
2022-01-14 16:01:30 -0600

    uprava struktury GUI, pridani Treeview a Menu

--- mcnp_tally_plot.py
+++ mcnp_tally_plot.py
@@ -2,16 +2,26 @@
 # TODO_list:
 
 # Libraries
-import os
+import os, sys
+
 import tkinter
-from tkinter import ttk
-from tkinter import messagebox
-from tkinter import simpledialog
-# from tkinter import filedialog
-# from pathlib import Path
-# import sys
+from tkinter import ttk, messagebox, simpledialog, filedialog, Menu
+from ttkwidgets import CheckboxTreeview
+
+from pathlib import Path
+
 
 #  FUNCTIONS  ##########################################################################################################
+
+
+def open_folder():
+    folder_path = Path(filedialog.askdirectory(title='Choose directory with input files', initialdir=os.getcwd()))
+
+    output_files = []
+    for file in os.listdir(folder_path):
+        output_files.append(Path(file))
+
+    print(output_files)
 
 
 # GUI exit from program
@@ -21,15 +31,12 @@ def ask_quit():
 
 
 #  MAIN CODE  ##########################################################################################################
-# TODO vlozit dve okna, do kazdeho framu jedno, přidat posuvniky
+# TODO vlozit okno pro log
 # TODO hlavicka tabulky v prvnim okne
-# TODO pridat horni listu (hlavne File...)
 
 
-# Global variables
-folder_path = None
-
-
+# ----------------------------------------------------------------------------------------------------------------------
+# main window creation
 root = tkinter.Tk()
 
 # Frames
@@ -38,43 +45,72 @@ down_frame = ttk.Frame(root)
 
 # Main window parameters
 root.title('MCNP tally plotting')
-# root.minsize(500, 300)
+root.minsize(500, 300)
 # root.maxsize(1000, 600)
 # root.geometry('800x350')
 style = ttk.Style()
 style.theme_use('vista')
+root.protocol('WM_DELETE_WINDOW', ask_quit)  # program end
 
+# ----------------------------------------------------------------------------------------------------------------------
+# treeview
+treeview_files = ttk.Treeview(up_frame)
+treeview_files['show'] = 'headings'
 
-# UP frame in GUI
+# Treeview X-scrollbar
+tree_x_scroll = ttk.Scrollbar(root, orient='horizontal')
+tree_x_scroll.configure(command=treeview_files.xview)
+treeview_files.configure(xscrollcommand=tree_x_scroll.set)
+# Treeview Y-scrollbar
+tree_y_scroll = ttk.Scrollbar(root, orient='vertical', command=treeview_files.yview)
+treeview_files.configure(yscrollcommand=tree_y_scroll.set)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# widget MENU
+# widget definition
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+# File in MENU definition
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='File', menu=file_menu)
+file_menu.add_command(label='Open work directory', underline=0, command=lambda: open_folder())
+
+# ----------------------------------------------------------------------------------------------------------------------
+# widgets in FRAMEs
+# widgets in UP frame in GUI
 label_layer_types = ttk.Label(up_frame, text='Nejaky text')
 
+# widgets DOWN frame in GUI
+button_solve = ttk.Button(down_frame, text='Close', command=ask_quit, width=20)
 
-# DOWN frame in GUI
-button_solve = ttk.Button(down_frame, text='PLOT', command=lambda: quit(), width=20)
-
+# ----------------------------------------------------------------------------------------------------------------------
 # GRIDs
+# frames in main GRID
 up_frame.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)     # set the margins between window and content
 down_frame.grid(column=0, row=1, sticky='nswe', padx=5, pady=5)
 
 # UP frame widgets grid
-label_layer_types.grid(sticky='W', column=0, row=0)
-label_layer_types.config(width=20)
+treeview_files.grid(sticky='wens', column=0, columnspan=5, row=0, rowspan=5)
+
 
 # DOWN frame widgets grid
-button_solve.grid(sticky='W', column=1, row=0)
+button_solve.grid(sticky='W', column=0, row=0)
 
+# ----------------------------------------------------------------------------------------------------------------------
+# LAYOUTs
+# layout UP frame
+up_frame.columnconfigure(0, weight=1)
+up_frame.rowconfigure(0, weight=1)
+
+# layout DOWN frame
+down_frame.columnconfigure(0, weight=1)
+down_frame.rowconfigure(0, weight=1)
+
+# layout all of the main containers
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-up_frame.columnconfigure(0, weight=1)
-up_frame.rowconfigure(0, weight=0)
-
-down_frame.columnconfigure(0, weight=1)
-down_frame.rowconfigure(1, weight=0)
-
-
-# program end
-root.protocol('WM_DELETE_WINDOW', ask_quit)
-
+# ----------------------------------------------------------------------------------------------------------------------
 # run UI
 root.mainloop()
