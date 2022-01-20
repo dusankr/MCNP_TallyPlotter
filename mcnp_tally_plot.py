1d0397d076aa01a7b64b1997778222fdc2aafd1a
dusankr <kral.dusan0@gmail.com>
2022-01-20 11:00:52 -0600

    zrušení wild cards, vše z tkinter má před sebou tk.atd...

--- mcnp_tally_plot.py
+++ mcnp_tally_plot.py
@@ -6,8 +6,7 @@ import os, sys
 
 from modules import config_mod, mcnp_read
 
-import tkinter
-from tkinter import ttk, messagebox, simpledialog, filedialog, Menu
+import tkinter as tk
 from ttkwidgets import CheckboxTreeview
 
 from pathlib import Path
@@ -15,7 +14,7 @@ from pathlib import Path
 
 #  FUNCTIONS  ##########################################################################################################
 def open_folder():
-    folder_path = Path(filedialog.askdirectory(title='Choose directory with input files', initialdir=Path.cwd()))
+    folder_path = Path(tk.filedialog.askdirectory(title='Choose directory with input files', initialdir=Path.cwd()))
 
     output_files = []
     for file in os.listdir(folder_path):
@@ -48,7 +47,7 @@ def treeview_fill():
 
 # GUI exit from program
 def ask_quit():
-    if messagebox.askokcancel('Quit', 'Do you want to quit now?'):
+    if tk.messagebox.askokcancel('Quit', 'Do you want to quit now?'):
         root.destroy()
 
 
@@ -58,52 +57,52 @@ def ask_quit():
 #
 # ----------------------------------------------------------------------------------------------------------------------
 # main window creation
-root = tkinter.Tk()
+root = tk.Tk()
 
 # Frames
-up_frame = ttk.Frame(root)
-down_frame = ttk.Frame(root)
+up_frame = tk.ttk.Frame(root)
+down_frame = tk.ttk.Frame(root)
 
 # Main window parameters
 root.title('MCNP tally plotting')
 root.minsize(500, 300)
 # root.maxsize(1000, 600)
 # root.geometry('800x350')
-style = ttk.Style()
+style = tk.ttk.Style()
 style.theme_use('vista')
 root.protocol('WM_DELETE_WINDOW', ask_quit)  # program end
 
 # ----------------------------------------------------------------------------------------------------------------------
 # treeview
-treeview_files = ttk.Treeview(up_frame)
+treeview_files = tk.ttk.Treeview(up_frame)
 treeview_files['show'] = 'headings'
 
 # Treeview X-scrollbar
-tree_x_scroll = ttk.Scrollbar(root, orient='horizontal')
+tree_x_scroll = tk.ttk.Scrollbar(root, orient='horizontal')
 tree_x_scroll.configure(command=treeview_files.xview)
 treeview_files.configure(xscrollcommand=tree_x_scroll.set)
 # Treeview Y-scrollbar
-tree_y_scroll = ttk.Scrollbar(root, orient='vertical', command=treeview_files.yview)
+tree_y_scroll = tk.ttk.Scrollbar(root, orient='vertical', command=treeview_files.yview)
 treeview_files.configure(yscrollcommand=tree_y_scroll.set)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # widget MENU
 # widget definition
-menu_bar = Menu(root)
+menu_bar = tk.Menu(root)
 root.config(menu=menu_bar)
 
 # File in MENU definition
-file_menu = Menu(menu_bar, tearoff=0)
+file_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='Open work directory', underline=0, command=lambda: open_folder())
 
 # ----------------------------------------------------------------------------------------------------------------------
 # widgets in FRAMEs
 # widgets in UP frame in GUI
-label_layer_types = ttk.Label(up_frame, text='Nejaky text')
+label_layer_types = tk.ttk.Label(up_frame, text='Nejaky text')
 
 # widgets DOWN frame in GUI
-button_solve = ttk.Button(down_frame, text='Close', command=ask_quit, width=20)
+button_solve = tk.ttk.Button(down_frame, text='Close', command=ask_quit, width=20)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # GRIDs
