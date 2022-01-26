9400b2e837f8fc98c05fe691cb7c1fc8b9c202a1
dusankr <kral.dusan0@gmail.com>
2022-01-25 22:20:11 -0600

    1. radio buttons jsou k sobe sparovany 2. option menu maji nabidku
    Problemy
    1. radio buttons nejsou graficky vybrane, realne ano
    2. tlacitka jsou divne zarovnana

--- modules/plot_mod.py
+++ modules/plot_mod.py
@@ -7,6 +7,7 @@
 # TODO
 # TODO stejná barva chybových úseček jako schodového grafu
 # TODO ošetřit když chybí data
+# TODO ratio plot -> aktivovat jen když je zvoleno víc tally
 
 # libraries
 import matplotlib.pyplot as plt     # ploting in matlab style
@@ -27,16 +28,32 @@ def plot_window(root, treeview_file, selected):
     new_win.title('Plotting window')
     new_win.minsize(100, 150)
 
+    # Tkinter variables
+    legend_options = ['best', 'upper right', 'upper left', 'lower left', 'lower right', 'right', 'center left', 'center right', 'lower center', 'upper center', 'center']
+    legend_pos = tk.StringVar()      # Option Menu variable
+    legend_pos.set(legend_options[0])
+
+    tally_to_plot = get_selected(treeview_file, selected)  # obtain keys from checkedbox treeview
+
+    if len(tally_to_plot) > 1:
+        ratio_options = ['no ratio'] + tally_to_plot
+    else:
+        ratio_options = ['no ratio']
+
+    x_axis_var = tk.StringVar(value='lin')     # radio button variable
+    y_axis_var = tk.StringVar(value='lin')  # radio button variable
+    data_var = tk.StringVar(value='norm')  # radio button variable
+
+    ratio_sel = tk.StringVar()       # Option Menu variable
+
     plot_frame = tk.ttk.Frame(new_win)
     plot_frame.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)  # set the margins between window and content
 
     plot_option_frame = tk.ttk.Frame(new_win)
     plot_option_frame.grid(column=1, row=0, sticky='nswe', padx=5, pady=5)
 
-    tally_to_plot = get_selected(treeview_file, selected)   # obtain keys from checkedbox treeview
-
     # Canvas for plot
-    canvas = FigureCanvasTkAgg(plot_function(tally_to_plot), plot_frame)    # add Figure to canvas from plot function
+    canvas = FigureCanvasTkAgg(plot_function(tally_to_plot, legend_pos.get()), plot_frame)    # add Figure to canvas from plot function
     canvas.draw()
     canvas.get_tk_widget().grid(column=0, row=0, sticky='nswe')
 
@@ -51,49 +68,52 @@ def plot_window(root, treeview_file, selected):
     x_axis_frame = tk.LabelFrame(plot_option_frame, text='X axis settings')
     x_axis_frame.grid(column=0, row=0, sticky='nwe', padx=5, pady=5)
 
-    x_lin_radio = tk.Radiobutton(x_axis_frame, text='lin')
+    x_lin_radio = tk.Radiobutton(x_axis_frame, text='lin', variable=x_axis_var, value='lin', tristatevalue="x", command=None)
     x_lin_radio.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)
-    x_log_radio = tk.Radiobutton(x_axis_frame, text='log')
+    x_log_radio = tk.Radiobutton(x_axis_frame, text='log', variable=x_axis_var, value='log', tristatevalue="x", command=None)
     x_log_radio.grid(column=1, row=0, sticky='nswe', padx=5, pady=5)
 
     # Y AXIS Radio Button
     y_axis_frame = tk.LabelFrame(plot_option_frame, text='Y axis settings')
     y_axis_frame.grid(column=0, row=1, sticky='nwe', padx=5, pady=5)
 
-    y_lin_radio = tk.Radiobutton(y_axis_frame, text='lin')
+    y_lin_radio = tk.Radiobutton(y_axis_frame, text='lin', variable=y_axis_var, value='lin', tristatevalue="y")
     y_lin_radio.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)
-    y_log_radio = tk.Radiobutton(y_axis_frame, text='log')
+    y_log_radio = tk.Radiobutton(y_axis_frame, text='log', variable=y_axis_var, value='log', tristatevalue="y")
     y_log_radio.grid(column=1, row=0, sticky='nswe', padx=5, pady=5)
 
     # DATA Radio Button
     data_inp_frame = tk.LabelFrame(plot_option_frame, text='Data input')
     data_inp_frame.grid(column=0, row=2, sticky='n', padx=5, pady=5)
 
-    data_inp_radio = tk.Radiobutton(data_inp_frame, text='norm')
+    data_inp_radio = tk.Radiobutton(data_inp_frame, text='norm', variable=data_var, value='norm', tristatevalue="z")
     data_inp_radio.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)
-    data_inp_radio = tk.Radiobutton(data_inp_frame, text='non')
+    data_inp_radio = tk.Radiobutton(data_inp_frame, text='non', variable=data_var, value='non', tristatevalue="z")
     data_inp_radio.grid(column=1, row=0, sticky='nswe', padx=5, pady=5)
 
     # LEGEND position
     legend_frame = tk.LabelFrame(plot_option_frame, text='Legend position')
     legend_frame.grid(column=0, row=3, sticky='nswe', padx=5, pady=5)
 
-    # option menu variables
-    legend_options = ['best', 'upper right', 'upper left', 'lower left', 'lower right', 'right', 'center left', 'center right', 'lower center', 'upper center', 'center']
-    legend_pos = tk.StringVar(new_win)
-    legend_pos.set('best')
-
-    legend_menu = tk.OptionMenu(legend_frame, legend_pos, *legend_options)
+    legend_menu = tk.ttk.OptionMenu(legend_frame, legend_pos, legend_options[0], *legend_options)   # plot_window(root, treeview_files, treeview_files.get_checked()
     legend_menu.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)
+
+    # Plot TO Plot ratio
+    ratio_frame = tk.LabelFrame(plot_option_frame, text='Ratio plot')
+    ratio_frame.grid(column=0, row=4, sticky='nswe', padx=5, pady=5)
+
+    ratio_menu = tk.ttk.OptionMenu(ratio_frame, ratio_sel, ratio_options[0], *ratio_options)  # plot_window(root, treeview_files, treeview_files.get_checked()
+    ratio_menu.grid(column=0, row=0, sticky='nswe', padx=5, pady=5)
 
     # GRID on/off ?
 
     # Buttons
-    button_replot = tk.ttk.Button(plot_option_frame, text='Replot', width=20)
-    button_replot.grid(column=0, row=4)
+    button_replot = tk.ttk.Button(plot_option_frame, text='Replot', width=20, command=lambda: plot_function(tally_to_plot, legend_pos.get()))
+    button_replot.grid(column=0, row=5)
 
-    button_export = tk.ttk.Button(plot_option_frame, text='Export to CSV', width=20)
-    button_export.grid(column=0, row=5)
+    # button_test = tk.ttk.Button(plot_option_frame, text='test', width=20, command=lambda: test_func(canvas))
+    # button_test.grid(column=0, row=5)
+
 
 
 
@@ -121,6 +141,8 @@ def get_selected(treeview_file, selected):
     return selection
 
 
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # support PLOT functions
 
@@ -134,7 +156,7 @@ def interval_mid(x):
 
 
 # plot tallies from user
-def plot_function(tally_to_plot):
+def plot_function(tally_to_plot, leg):
     fig = plt.figure()
 
     for name in config_mod.tallies.keys():
@@ -151,7 +173,7 @@ def plot_function(tally_to_plot):
 
 
     # todo zformátovat graf
-    plt.legend(loc='upper right')
+    plt.legend(loc=leg)
     plt.grid()
     plt.xlabel('energy (MeV)')
     plt.ylabel('average flux in cell per one generated neutron')
