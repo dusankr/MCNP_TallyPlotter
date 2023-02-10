541496497fd18a6e5474c7eb87448ebdb9cef219
dusankr <>
2023-02-10 13:51:38 -0700

    test2

--- mcnp_tally_plot_main.py
+++ mcnp_tally_plot_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # TODO_list:
-# something very important (commit and push test at GitHub)
+# comment deleted
 
 # LIBRARIES
 from modules import read_mod, plot_mod, config_mod
